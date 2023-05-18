f5e45fcfc6fc4bd2aa4c16395979d4bddf5dd32a
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-05-18 17:40:37 +0530

    Initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,25 @@
+// values 
+    // primitive datatypes
+// number : 1,1.5,24
+// boolean : true, false 
+// String: "hello" or 'hello'
+// null
+// undefined
+
+    // non primitive datatypes
+// Array:[1,1.5,24,'hello', true]
+// Object: {}
+
+// Keywords
+
+// var is a variable that is obsolete now, we let instead of var
+// let highScore = 200;
+// const myHighScore = 200;
+
+// Lets take a question
+
+let highScore = 200;
+let myHighScore = "200";
+
+console.log(highScore == myHighScore);  
+// It will print true because javascript don't check datatypes in double equal
