037a22b6e297a8425ed515f1c27de3b6465c634c
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-07-27 18:52:14 +0530

    Upload ifElse.js

---
+++ JavaScript/ifElse.js
@@ -0,0 +1,12 @@
+// if else
+// let a = 5;
+// let b = 9;
+// if(a > b){
+//     console.log(a,"is greater.");
+// }
+// else if(a == b){
+//     console.log(a , "is equal to", b);
+// }
+// else{
+//     console.log(b,"is greater.");
+// }
