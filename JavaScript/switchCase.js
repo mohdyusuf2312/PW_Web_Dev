87e939757a05ad3da93ac26d0605925966673319
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-08-26 18:59:02 +0530

    Upload switchCase.js

---
+++ JavaScript/switchCase.js
@@ -0,0 +1,14 @@
+let user = "a";
+switch (user) {
+    case "admin":
+        console.log("He is admin");
+        break;
+    case "student":
+        console.log("He is student");
+        break;
+    case "mentor":
+        console.log("He is mentor");
+        break;
+    default:
+        console.log("He is nothing");
+}
