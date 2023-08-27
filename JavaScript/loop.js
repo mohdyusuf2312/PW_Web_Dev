83b115340e01138810abf0df3b0c83f371553298
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-08-27 19:10:01 +0530

    Upload loop.js

---
+++ JavaScript/loop.js
@@ -0,0 +1,23 @@
+// do-while loop
+
+let i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i < 5);
+
+
+//  while-loop
+
+let j = 0;
+while(j < 5){
+    console.log(j);
+    j++;
+}
+
+
+// for loop
+
+for(let i = 0; i < 5; i++){
+    console.log(i);
+}
