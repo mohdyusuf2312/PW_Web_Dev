e3497025882a9cebef09c07d358d0db3d84bbaa9
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-10-11 15:18:38 +0530

    Upload comparision.js

---
+++ JavaScript/comparision.js
@@ -0,0 +1,7 @@
+let a = 3;     //Type: Number
+let b = new Number(3); //This is Number object   //type:object
+let c = 3;      //type: number
+
+console.log(a == b);  true
+console.log(a === b); false //different types
+console.log(b === c); false //different types
