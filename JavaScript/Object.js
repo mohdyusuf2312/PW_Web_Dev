1d0c0b20671474a73791b1f86bebf6394e9c7676
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-10-12 12:11:26 +0530

    Upload object.js

---
+++ JavaScript/Object.js
@@ -0,0 +1,6 @@
+let greetings;
+greetings = {}; // it will print empty object : output : {}
+console.log(greetings);
+
+// let greetings = { a: "hello", b: "yello", c: 123};
+// console.log(greetings);
