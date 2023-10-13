ac6365514fe178ee00c70f1b9b6265ac0c1295d0
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-10-13 12:17:34 +0530

    Upload object.js

---
+++ JavaScript/eval.js
@@ -0,0 +1,5 @@
+const sum = eval("10*10+5");  //Here we use eval: it use for treating string as integer.
+console.log(sum); // output: 105 
+
+
+// multiplication/division operator has more priority than addition/subtraction operation.
