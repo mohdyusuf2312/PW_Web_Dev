3206e091b3843f3c235bbf533c1199e81fba4b64
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-12-01 12:27:38 +0530

    Upload typeOf.js

---
+++ JavaScript/typeOf.js
@@ -0,0 +1,7 @@
+console.log(typeof 1); // this will print number.
+
+console.log(typeof typeof 1); // this will print string beacuse "typeof 1" gives a string that is "number" second typeof is like (typeof number) => string
+
+console.log(typeof number); // this will produce undefined
+
+// console.log(typeof number); != console.log(typeof "number");
