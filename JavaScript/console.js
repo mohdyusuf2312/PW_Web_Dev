8e61121eac51558b8c597929fc3d83709fe38a9c
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-12-03 13:03:11 +0530

    Add OR operator with many elements

--- JavaScript/console.js
+++ JavaScript/console.js
@@ -1,4 +1,12 @@
 console.log("Welcome"[0]); // This will print first character.
 
-let name = "Hello";
-console.log(name()); // this is type error
+// let name = "Hello";
+// console.log(name()); // this is type error
+
+let one = false || {} || null;    // null is falsy
+let two = null || false || "";    // flase is also falsy
+let three = [] || {} || null;     // "" empty string is also falsy
+
+                                  // true, {}, [] all are truthy
+console.log(one, two, three);     // ||(OR operator) checks first truly element, its checks from left to right
+                                  // if no one is truthy it will assign last element like in two variable all are falsy (it assign "")
