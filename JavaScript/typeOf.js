68dcc9097591a35d82bc95f8753b52c3a14cdd76
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-12-06 13:48:27 +0530

    Add example of === comparison

--- JavaScript/typeOf.js
+++ JavaScript/typeOf.js
@@ -5,3 +5,13 @@ console.log(typeof typeof 1); // this will print string beacuse "typeof 1" gives
 console.log(typeof number); // this will produce undefined
 
 // console.log(typeof number); != console.log(typeof "number");
+
+
+const name = "PW Skills";
+
+console.log(!typeof name === "objects");    // false
+console.log(!typeof name === "string");     // false
+
+// (typeof name) gives "string"
+// here is ! mark that means not string on the left side
+// while right side is string so the comparision are false
