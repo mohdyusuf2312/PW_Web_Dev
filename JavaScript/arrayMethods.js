de407bcbcf03472ccbcd9c9d625a4cfe6a354518
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-12-09 12:09:51 +0530

    Add some syntax

--- JavaScript/arrayMethods.js
+++ JavaScript/arrayMethods.js
@@ -23,3 +23,21 @@ console.log(f); // it will print 1
 
 // (push, unshift) 
 console.log(g); // it will print total number of element after adding yusuf string
+
+// concatinate
+let a = [1, 2, 3];
+let b = [4, 5, 6];
+let c = a.concat(b);
+console.log(c); // this will print [1, 2, 3, 4, 5, 6]
+
+// join 
+let d = a.join("#");
+console.log(d); // it will print a string of array a with seperated by # e.g 1#2#3
+
+// reverse 
+let e = a.reverse();
+console.log(e); // it will print reverse array a, e.g ([3, 2, 1])
+
+let x = [1, 2, 3, 4, "Hello", true, 5, 60];
+let y = x.slice(3, 7); // y =  [4, "Hello", true, 5]
+console.log(y);
