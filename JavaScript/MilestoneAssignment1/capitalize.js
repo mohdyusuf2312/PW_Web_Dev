065bd3d8f441a87dbb9d9c623bf3857aa2791568
Muhammad Yusuf <mohdyusufr@gmail.com>
2023-12-19 17:27:55 +0530

    Upload file

---
+++ JavaScript/MilestoneAssignment1/capitalize.js
@@ -0,0 +1,7 @@
+let name = "yusuf";
+let count = 0;
+for (let i = 0; i < name.length; i++){
+    if(name[i] === 'a' || name[i] === 'e' || name[i] === 'i' || name[i] === 'o' || name[i] === 'u' || name[i] === 'A' || name[i] === 'E' || name[i] === 'I' || name[i] === 'O' || name[i] === 'U') count++;
+}
+console.log(count);
+
