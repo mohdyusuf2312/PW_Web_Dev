daa90c44f0763cc491543a556b28abe953326846
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-05-01 12:01:29 +0530

    Update methods

--- JavaScript/arrayMethods2.js
+++ JavaScript/arrayMethods2.js
@@ -51,5 +51,21 @@ console.log(arr.some((ele) => {
 
 console.log(arr.indexOf(310));
 
+// lastIndexof 
+
 console.log(arr.lastIndexOf("str"));    // same work but it will check from end of the array.
 
+// find 
+
+console.log(arr.find((ele) => ele > 200));
+
+// reduce           // reduce work on two parameter one is previous value and another one is current value. 
+
+console.log(arr.reduce((previous, current) => {
+    return previous + current;
+},0));                  // here zero is previous value at iteration 1.
+
+// console.log(arr.sort((a,b) => a - b));
+console.log(arr.sort());
+
+console.log(arr.findIndex((ele)  => ele > 210));
