892d9cfc881882b5a3c0a66acb5a4730f740dff4
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-05-28 11:10:41 +0530

    upload spread.js

---
+++ JavaScript/MOD-43/spread.js
@@ -0,0 +1,6 @@
+const oneArray = [1,2,3,4];
+const twoArray = [5,6,7,8];
+
+// const threeArray = oneArray.concat(twoArray);
+const threeArray = [...oneArray, ...twoArray];
+console.log(threeArray);
